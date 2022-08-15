97ed458dc21593314b4560d203d51f1342d50cbd
«Kristina» <kristinabol1996@gmail.com>
2022-08-16 01:16:25 +0800

    1 задача

---
+++ Task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа, которая принимает на вход
+// трехзначное число и на выходе показывает
+// вторую цифру этого числа
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = number % 100;
+Console.WriteLine(digit / 10);
